90467e11c78cd93faa57541510db51e68abb58d8
nickboon <boonnick@hotmail.com>
2021-01-17 01:29:03 +0000

    Initial commit.

---
+++ js/about/getContent.js
@@ -0,0 +1,6 @@
+(function () {
+	var content = 'About content';
+	console.log(content);
+
+	document.getElementById('content').innerHTML = content;
+})();
---
+++ js/index/getContent.js
@@ -0,0 +1,6 @@
+(function () {
+	var content = 'Index content';
+	console.log(content);
+
+	document.getElementById('content').innerHTML = content;
+})();
